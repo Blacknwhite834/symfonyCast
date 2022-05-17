785dadc93877064b9176482b4b6b93ea508fb7d5
Evan Le coz <evanlecoz6@gmail.com>
2022-05-17 22:08:17 +0200

    Fin du cours

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b9516676a4d0f6cf62033930385da232c957e7fb',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b9516676a4d0f6cf62033930385da232c957e7fb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
